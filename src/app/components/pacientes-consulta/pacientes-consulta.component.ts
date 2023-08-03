0796ea8cd7966872bbfcce76afe581052e858da2
marcelogmoura <mgmoura@gmail.com>
2023-08-03 20:03:19 -0300

    consulta-paciente e mascaras

--- src/app/components/pacientes-consulta/pacientes-consulta.component.ts
+++ src/app/components/pacientes-consulta/pacientes-consulta.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-pacientes-consulta',
   templateUrl: './pacientes-consulta.component.html',
   styleUrls: ['./pacientes-consulta.component.css']
 })
-export class PacientesConsultaComponent {
+export class PacientesConsultaComponent implements OnInit {
+
+  pacientes: any[] = [];
+
+  constructor(
+    private HttpClient: HttpClient
+  ){
+  }
+
+  ngOnInit(): void {
+    this.HttpClient.get('http://localhost:8081/api/pacientes')
+      .subscribe({
+        next: (data) => {
+          this.pacientes = data as any[];
+        },
+        error: (e) => {
+          console.log(e);
+        }
+
+      })
+
+
+  }
+  // mascara ok
+  formatarTel(telefone: string): string {
+    const telFormatado = telefone.replace(/^(\d{4})(\d{4})$/, '$1-$2');
+    return telFormatado;
+  }
 
 }
